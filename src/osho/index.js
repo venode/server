4d7007b55c42fc0eafc8d52ed503611be55d99ba
Andy Chen <amazingandyyy@gmail.com>
2018-09-11 17:50:46 -0700

    prototype osho

--- src/osho/index.js
+++ src/osho/index.js
@@ -4,7 +4,13 @@ import config from './config';
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('connect'));
-router.get('/testnet/:token', (req, res) => res.redirect(config.TESTNET_SOURCE));
+router.get('/testnet', (req, res) => {
+  res.send('no token');
+});
+router.get('/testnet/:token', (req, res) => {
+  if (!req.params.token) return res.send('no token');
+  return res.redirect(307, `${config.TESTNET_SOURCE}/blocks/best`);
+});
 router.get('/mainnet/:token', (req, res) => res.redirect(config.MAINNET_SOURCE));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import osho from './osho';
-import config, { PORT } from './config';
+import { PORT } from './config';
+import oshoConfig from './osho/config';
 
 const app = express();
 
 app.get('/ping', (req, res) => res.send('pong'));
 app.use('/connect', osho);
 
-app.get('/', (req, res) => res.send(config.docs));
+app.get('/', (req, res) => res.redirect(oshoConfig.MAINNET_SOURCE));
 
-app.listen(PORT);
-console.log(`Listening on ${PORT}`); /* eslint-disable-line */
+app.listen(PORT, () => console.log(`Listening on ${PORT}`)); /* eslint-disable-line */
+app.listen(PORT + 5, () => console.log(`Listening on ${PORT+5}`)); /* eslint-disable-line */
